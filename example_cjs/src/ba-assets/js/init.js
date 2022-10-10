f8ac897c2132b7ce179ae5a55c3c6b0fb58a248e
liaohui1080 <liaohui1080@tianliaohui.com>
2022-10-10 10:27:38 +0800

    feat: 第一次提交

--- example_cjs/src/ba-assets/js/init.js
+++
@@ -1,6 +0,0 @@
-Vue.component("panel", httpVueLoader("./components/panel.vue")); //注册一个全局组件
-Vue.component("chart", httpVueLoader("./components/chart.vue")); //注册一个全局组件
-Vue.component("kuang", httpVueLoader("./components/kuang.vue")); //注册一个全局组件
-Vue.component("ba-progress", httpVueLoader("./components/progress.vue")); //注册一个全局组件
-Vue.component("ba-diagnose-list", httpVueLoader("./components/diagnoseList.vue")); //诊断列表
-
